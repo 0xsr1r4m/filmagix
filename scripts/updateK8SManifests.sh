d524d3bdd06fb99d250f02e0d458b3f012529e33
Sriram Kumpatla <Sriram.Kumpatla+CPSI@trubridge.com>
2025-12-05 09:27:53 -0500

    test11

--- scripts/updateK8SManifests.sh
+++ scripts/updateK8SManifests.sh
@@ -9,6 +9,8 @@ set -ex
 
 REPO_URL="https://${AZURE_DEVOPS_ORG}:${AZURE_DEVOPS_PAT}@dev.azure.com/${AZURE_DEVOPS_ORG}/${AZURE_DEVOPS_PROJECT}/_git/${AZURE_DEVOPS_REPO}"
 
+rm -rf /tmp/temp_repo/*
+
 git clone "$REPO_URL" /tmp/temp_repo
 
 cd /tmp/temp_repo
@@ -21,4 +23,4 @@ git commit -m "Update Kubernetes manifest to $2:$3"
 
 git push
 
-rm -rf /tmp/temp_repo
+rm -rf /tmp/temp_repo/*
