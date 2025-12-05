ec9172cdfe9c083093f94d93f1e51f9449e45df0
Sriram Kumpatla <Sriram.Kumpatla+CPSI@trubridge.com>
2025-12-05 07:48:25 -0500

    test6

--- scripts/updateK8SManifests.sh
+++ scripts/updateK8SManifests.sh
@@ -9,7 +9,7 @@ set -ex
 
 REPO_URL="https://${AZURE_DEVOPS_ORG}:${AZURE_DEVOPS_PAT}@dev.azure.com/${AZURE_DEVOPS_ORG}/${AZURE_DEVOPS_PROJECT}/_git/${AZURE_DEVOPS_REPO}"
 
-rm -rf /tmp/temp_repo
+rm -rf /tmp/temp_repo/*
 
 git clone "$REPO_URL" /tmp/temp_repo
 
